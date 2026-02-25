f0b906f58fe5db5cbefcdba72fab9dfa197d3065
AmanMirza9898 <aapka-email@example.com>
2026-02-25 13:46:45 +0530

    Implement server-side pagination for All Applicants view

--- frontend/src/redux/applicationSlice.js
+++ frontend/src/redux/applicationSlice.js
@@ -4,13 +4,21 @@ const applicationSlice = createSlice({
     name:"applications",
     initialState:{
         applications: [],
+        pagination: {
+            totalApplications: 0,
+            totalPages: 0,
+            currentPage: 1
+        }
     },
     reducers:{
         setAllApplications:(state,action) => {
             state.applications = action.payload;
         },
+        setPagination:(state,action) => {
+            state.pagination = action.payload;
+        }
     },
 });
 
-export const {setAllApplications} = applicationSlice.actions;
+export const {setAllApplications, setPagination} = applicationSlice.actions;
 export default applicationSlice.reducer;
